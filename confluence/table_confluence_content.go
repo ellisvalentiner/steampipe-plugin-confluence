552e7ec80db878d82f964826d8f829009da6be5b
Ellis Valentiner <ellisvalentiner@users.noreply.github.com>
2022-01-03 15:34:15 -0600

    fix: support limit clause (#12)

--- confluence/table_confluence_content.go
+++ confluence/table_confluence_content.go
@@ -107,23 +107,37 @@ func listContent(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData
 		return nil, err
 	}
 
+	var maxResults int
+	limit := d.QueryContext.Limit
+	if limit != nil {
+		if *limit < int64(100) {
+			maxResults = int(*limit)
+		}
+	} else {
+		maxResults = 100
+	}
+
 	startAt := 0
-	maxResults := 50
+	// maxResults := 50
 
 	options := &confluence.GetContentOptionsScheme{
 		Expand: []string{"childTypes.all", "body.storage"},
 	}
 
-	for {
+	pagesLeft := true
+	for pagesLeft {
 		page, _, err := instance.Content.Gets(context.Background(), options, startAt, maxResults)
 		if err != nil {
 			return nil, err
 		}
 		for _, content := range page.Results {
 			d.StreamListItem(ctx, content)
+			if plugin.IsCancelled(ctx) {
+				return  nil, nil
+			}
 		}
 		if page.Size < page.Limit {
-			break
+			pagesLeft = false
 		}
 		startAt += maxResults
 	}
