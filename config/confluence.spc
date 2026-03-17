84678a40b795ef0a1d830ca6e856279cdf54d52f
vthiery <vincent.thiery@camunda.com>
2026-03-17 16:33:46 +0100

    feat: add support for Confluence Data Center configuration and authentication

--- config/confluence.spc
+++ config/confluence.spc
@@ -1,13 +1,17 @@
 connection "confluence" {
-    plugin    = "ellisvalentiner/confluence"
+    plugin = "ellisvalentiner/confluence"
 
+    # --- Confluence Cloud (default) ---
+    # deployment_type = "cloud"   # default; can be omitted
     # Base URI of your Confluence Cloud instance
-    base_url = "https://your-domain.atlassian.net/"
-
+    # base_url = "https://your-domain.atlassian.net/"
     # The user name to access the Confluence cloud instance
     # username = "name@company.domain"
+    # token    = "your-cloud-api-token" # Atlassian Cloud API token
 
-    # Access Token for the API
-    # See https://id.atlassian.com/manage/api-tokens
-    # token = ""
+    # --- Confluence Data Center / Server ---
+    # deployment_type = "datacenter"
+    # Base URI of your Confluence Datacenter instance
+    # base_url = "https://confluence.your-company.com/"
+    # token    = "your-data-center-PAT" # Personal Access Token; username not required
 }
--- confluence/utils.go
+++ confluence/utils.go
@@ -2,10 +2,24 @@ package confluence
 
 import (
 	"context"
+	"net/http"
+	"strings"
 
 	"github.com/ctreminiom/go-atlassian/confluence"
 	"github.com/turbot/steampipe-plugin-sdk/v4/plugin"
 )
+
+// dataCenterTransport rewrites the wiki/rest/api/ path prefix used by go-atlassian
+// (which targets Confluence Cloud) to /rest/api/ for Confluence Data Center.
+type dataCenterTransport struct {
+	wrapped http.RoundTripper
+}
+
+func (t *dataCenterTransport) RoundTrip(r *http.Request) (*http.Response, error) {
+	r = r.Clone(r.Context())
+	r.URL.Path = strings.Replace(r.URL.Path, "/wiki/rest/api/", "/rest/api/", 1)
+	return t.wrapped.RoundTrip(r)
+}
 
 func connect(_ context.Context, d *plugin.QueryData) (*confluence.Client, error) {
 
@@ -18,11 +32,25 @@ func connect(_ context.Context, d *plugin.QueryData) (*confluence.Client, error)
 	// Prefer config options given in Steampipe
 	confluenceConfig := GetConfig(d.Connection)
 
-	instance, err := confluence.New(nil, *confluenceConfig.BaseUrl)
+	isDataCenter := confluenceConfig.DeploymentType != nil && *confluenceConfig.DeploymentType == "datacenter"
+
+	var httpClient *http.Client
+	if isDataCenter {
+		httpClient = &http.Client{
+			Transport: &dataCenterTransport{wrapped: http.DefaultTransport},
+		}
+	}
+
+	instance, err := confluence.New(httpClient, *confluenceConfig.BaseUrl)
 	if err != nil {
 		return nil, err
 	}
-	instance.Auth.SetBasicAuth(*confluenceConfig.Username, *confluenceConfig.Token)
+
+	if isDataCenter {
+		instance.Auth.SetBearerToken(*confluenceConfig.Token)
+	} else {
+		instance.Auth.SetBasicAuth(*confluenceConfig.Username, *confluenceConfig.Token)
+	}
 	instance.Auth.SetUserAgent("curl/7.54.0")
 
 	// Save to cache
